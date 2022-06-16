cb064529d2b0f5f807a24681f408cb431c586b13
lmn159951 <61396224+lmn159951@users.noreply.github.com>
2022-06-16 16:35:51 +0700

    DangNhapController

--- app/Models/NhanVien.php
+++ app/Models/NhanVien.php
@@ -5,11 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class NhanVien extends Authenticatable
 {
-    use HasFactory, SoftDeletes;
+    use HasApiTokens, HasFactory, Notifiable;
     
     protected $primaryKey = 'ma_nhan_vien';
+    protected $fillable = [
+        'ten_tai_khoan',
+        'password',
+        'la_quan_tri'
+    ];
     protected $table = 'nhan_vien';
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 }
--- resources/views/nhanvien/dangnhap.blade.php
+++ resources/views/nhanvien/dangnhap.blade.php
@@ -1,1 +1,7 @@
 Đăng nhập
+<form action="{{ route('dangnhapPost') }}" method="POST">
+    @csrf
+    <input type="text" value="" placeholder="Tên đăng nhập" name="username">
+    <input type="password" value="" placeholder="Mật khẩu" name="password">
+    <button type="submit">Đăng nhập</button>
+</form>
