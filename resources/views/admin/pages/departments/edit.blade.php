5f9edd5078ab2b987f3a5ef137bb19546327b69f
bttd6 <107390835+bttd6@users.noreply.github.com>
2022-06-17 09:15:43 +0700

    Update Departments, Positions

--- resources/views/admin/pages/departments/edit.blade.php
+++ resources/views/admin/pages/departments/edit.blade.php
@@ -3,20 +3,21 @@
 @section('content')
     <div class="container-fluid">
         <div class="shadow p-4 mb-5 bg-body rounded">
-            <h1 class="text-center">Cập nhật chức vụ</h1>
+            <h1 class="text-center">Cập nhật phòng ban</h1>
 
             <form class="container" style="max-width: 800px;"
-                action="{{ route('quantri.chucvu.update', $chucVu->ma_chuc_vu) }}" method="POST">
+                action="{{ route('admin.departments.update', $department->id) }}" method="POST">
 
                 @method('PATCH')
                 @csrf
 
                 <div class="mb-3">
-                    <label for="ten_chuc_vu" class="form-label">Tên chức vụ:</label>
-                    <input type="text" class="form-control @error('ten_chuc_vu') is-invalid @enderror"
-                        value="{{ old('ten_chuc_vu') ?? $chucVu->ten_chuc_vu }}" name="ten_chuc_vu" id="ten_chuc_vu">
+                    <label for="name" class="form-label">Tên phòng ban:</label>
+                    <input type="text" class="form-control @error('name') is-invalid @enderror"
+                        value="{{ old('name') ?? $department->name }}" name="name"
+                        id="name">
 
-                    @error('ten_chuc_vu')
+                    @error('name')
                         <div id="validationServer03Feedback" class="invalid-feedback">
                             {{ $message }}
                         </div>
@@ -25,7 +26,7 @@
 
                 <div class="my-3 d-flex justify-content-end">
                     <button type="submit" class="btn btn-primary mr-3">Cập nhật</button>
-                    <a class="btn btn-secondary" href="{{ route('quantri.chucvu.index') }}">
+                    <a class="btn btn-secondary" href="{{ route('admin.departments.index') }}">
                         Trở về
                     </a>
                 </div>
--- resources/views/admin/pages/positions/edit.blade.php
+++ resources/views/admin/pages/positions/edit.blade.php
@@ -3,21 +3,20 @@
 @section('content')
     <div class="container-fluid">
         <div class="shadow p-4 mb-5 bg-body rounded">
-            <h1 class="text-center">Cập nhật phòng ban</h1>
+            <h1 class="text-center">Cập nhật chức vụ</h1>
 
             <form class="container" style="max-width: 800px;"
-                action="{{ route('quantri.phongban.update', $phongBan->ma_phong_ban) }}" method="POST">
+                action="{{ route('admin.positions.update', $positions->id) }}" method="POST">
 
                 @method('PATCH')
                 @csrf
 
                 <div class="mb-3">
-                    <label for="ten_phong_ban" class="form-label">Tên phòng ban:</label>
-                    <input type="text" class="form-control @error('ten_phong_ban') is-invalid @enderror"
-                        value="{{ old('ten_phong_ban') ?? $phongBan->ten_phong_ban }}" name="ten_phong_ban"
-                        id="ten_phong_ban">
+                    <label for="name" class="form-label">Tên chức vụ:</label>
+                    <input type="text" class="form-control @error('name') is-invalid @enderror"
+                        value="{{ old('name') ?? $positions->name }}" name="name" id="name">
 
-                    @error('ten_phong_ban')
+                    @error('name')
                         <div id="validationServer03Feedback" class="invalid-feedback">
                             {{ $message }}
                         </div>
@@ -26,7 +25,7 @@
 
                 <div class="my-3 d-flex justify-content-end">
                     <button type="submit" class="btn btn-primary mr-3">Cập nhật</button>
-                    <a class="btn btn-secondary" href="{{ route('quantri.phongban.index') }}">
+                    <a class="btn btn-secondary" href="{{ route('admin.positions.index') }}">
                         Trở về
                     </a>
                 </div>
